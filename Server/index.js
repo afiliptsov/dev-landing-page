52141ab6e6d187a4351718e8a98677ff24d80348
afiliptsov <filiptsovanton@gmail.com>
2019-01-09 00:40:27 -0600

    removed comment

--- Server/index.js
+++ Server/index.js
@@ -12,12 +12,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./index.js"));
 });
 
-// app.use(express.static(`${__dirname}/../build`));
-
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../build/index.html"));
-// });
-
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
